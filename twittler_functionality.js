5bde20344cb7084164a21de63cea8ec82f8695bd
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-01-12 19:08:44 -0800

    Styled divs to place them correctly on the page and added title and tagline

--- twittler_functionality.js
+++ twittler_functionality.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
         
         function getLatestTweetStream() {
           var $body = $('body');
-          $body.html('<div id="timeline">');
+          $body.html('<h1 id="title">twittler</h1><h4 id="tagline">We put the l in twitter</h4><div id="timeline">');
           var index = streams.home.length - 1;
           while(index >= 0){
             var tweet = streams.home[index];
             var $tweet = $('<div class="tweet">');
             $tweet.append('<div class="user">@' + tweet.user + '</div>');
-            $tweet.append('<div class="timestamp"> - ' + jQuery.timeago(tweet.created_at) + '</div>');
+            $tweet.append('<div class="timestamp">' + jQuery.timeago(tweet.created_at) + '</div>');
             $tweet.append('<div class="message">' + tweet.message + '</div>');
             $tweet.append('</div>');
             $tweet.appendTo($body);
